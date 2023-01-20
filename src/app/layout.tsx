5bafc51d81dfaab2a44b32641d0eb538c3bbf39d
msri2307 <srikanth23111995@gmail.com>
2023-01-20 18:12:31 +0530

    prettier check added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
-
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <head />
+            <body>{children}</body>
+        </html>
+    )
 }
